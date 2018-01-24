97fcf293b2fd0ab433fdc4314619c05c8d4e43a1
lonord <lonord@qq.com>
2018-01-24 09:38:13 +0800

    +

--- src/demo-net-get-interface.ts
+++ src/demo-net-get-interface.ts
@@ -2,7 +2,7 @@ import getInterface from '@lonord/net-get-interface'
 
 export default function run() {
 	return getInterface().then((list) => {
-		console.log('Got interface:')
+		console.log(`Got interface: (${list.length} items)`)
 		list.forEach((s) => console.log(`    ${s}`))
 	}).catch((err) => console.error(err))
 }
