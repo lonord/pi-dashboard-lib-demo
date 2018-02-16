16dfea22f2a86450e435eb77d816e11a811eb5bc
lonord <lonord@qq.com>
2018-02-16 14:16:12 +0800

    update api

--- src/demo-disk-util.ts
+++ src/demo-disk-util.ts
@@ -7,16 +7,15 @@ export default function run() {
 	}).then(() => {
 		console.log(`Got disk iostat:`)
 		return new Promise((resolve, reject) => {
-			let watcher: IOStatWatcher
 			let t = 0
-			watchIOStat(1000, (stats) => {
+			const watcher = watchIOStat(1000, (stats) => {
 				t++
 				console.log(JSON.stringify(stats, null, 4))
 				if (t > 3) {
 					watcher.stop()
 					resolve()
 				}
-			}).then((w) => watcher = w).catch(reject)
+			}, reject)
 		})
 	}).catch((err) => console.error(err))
 }
--- src/demo-net-traffic-monitor.ts
+++ src/demo-net-traffic-monitor.ts
@@ -8,7 +8,6 @@ export default function run() {
 		console.log(JSON.stringify(result, null, 4))
 	}).then(() => {
 		return new Promise((resolve, reject) => {
-			let watcher: TrafficSpeedWatcher
 			let count = 0
 			const onReport = (speed: TrafficSpeed) => {
 				count++
@@ -19,10 +18,7 @@ export default function run() {
 					resolve()
 				}
 			}
-			const onError = (err: any) => {
-				reject()
-			}
-			watchTrafficSpeed('br-lan', 3000, onReport).then((w) => watcher = w).catch(() => reject())
+			const watcher = watchTrafficSpeed('br-lan', 3000, onReport, reject)
 		})
 	})
 }
